3e09b117cbe46a6e2d21574748a0ab929d26d660
Gaurav Pareek <gaurav.pareek@Gaurav-pareek-m2.home>
2024-05-06 15:15:34 +0200

    added get method

--- service/src/routes/service.route.js
+++ service/src/routes/service.route.js
@@ -9,7 +9,7 @@ serviceRouter.post('/', async (req, res) => {
   res.send();
 });
 
-serviceRouter.post('/sayHello', async (req, res) => {
+serviceRouter.get('/sayHello', async (req, res) => {
   logger.info('Hello from Mindcurv');
   res.status(200);
   res.send('<p>Hello from Mindcurv.</p>');
